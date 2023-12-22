c26390cb0d3ed49a9cd28343baad709cf2adcbed
ჩხარტიშვილი ნიკოლოზი <chkhartishvili.n@itstep.ge>
2023-12-22 14:08:41 +0400

    Database connection and initial test migration

---
+++ NetworkMarketingManagement/NetworkManagmentAPI.Data/ApplicationDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace NetworkManagmentAPI.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+        }
+    }
+}
