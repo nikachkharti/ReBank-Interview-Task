895d2891839c492fa81045605bfaacefd9257b41
ჩხარტიშვილი ნიკოლოზი <chkhartishvili.n@itstep.ge>
2023-12-24 22:01:44 +0400

    Models project added with automapper

---
+++ NetworkMarketingManagement/NetworkMarketingManagement.API/MappingProfile.cs
@@ -0,0 +1,8 @@
+﻿using AutoMapper;
+
+namespace NetworkMarketingManagement.API
+{
+    public class MappingProfile : Profile
+    {
+    }
+}
--- NetworkMarketingManagement/NetworkMarketingManagement.API/Program.cs
+++ NetworkMarketingManagement/NetworkMarketingManagement.API/Program.cs
@@ -1,11 +1,16 @@
 using Microsoft.EntityFrameworkCore;
+using NetworkManagementAPI.Repository;
+using NetworkManagementAPI.Repository.Interfaces;
 using NetworkManagmentAPI.Data;
+using NetworkMarketingManagement.API;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("LocalSQLServerConnection")));
+builder.Services.AddScoped<IRepositoryFactory, RepositoryFactory>();
+builder.Services.AddAutoMapper(typeof(MappingProfile));
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
